20c264603754ce2256cd1b044c9b7debaf789d31
igamasayuki <igamasayuki@gmail.com>
2021-06-04 07:50:52 +0900

    toStringを戻しました

--- src/main/java/jp/co/runy/sample/Car.java
+++ src/main/java/jp/co/runy/sample/Car.java
@@ -1,33 +1,36 @@
 package jp.co.runy.sample;
 
 public class Car {
-  private int speed;
-  private String bodyColor;
+	private int speed;
+	private String bodyColor;
 
+	public Car() {
+	}
 
-  public Car() {
-  }
+	public Car(int speed2, String bodyColor) {
+		this.speed = speed2;
+		this.bodyColor = bodyColor;
+	}
 
-  public Car(int speed2, String bodyColor) {
-    this.speed = speed2;
-    this.bodyColor = bodyColor;
-  }
+	public int getSpeed() {
+		return this.speed;
+	}
 
-  public int getSpeed() {
-    return this.speed;
-  }
+	public void setSpeed(int speed) {
+		this.speed = 10;
+	}
 
-  public void setSpeed(int speed) {
-    this.speed = 10;
-  }
+	public String getBodyColor() {
+		return this.bodyColor;
+	}
 
-  public String getBodyColor() {
-    return this.bodyColor;
-  }
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 
-  public void setBodyColor(String bodyColor) {
-    this.bodyColor = bodyColor;
-  }
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+	}
 
-// 削除した
 }
