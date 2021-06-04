0d2af66f53ee7d2fc8cd3c97bff983bd4a4768df
igamasayuki <igamasayuki@gmail.com>
2021-06-04 17:27:29 +0900

    Revert "Revert "F/x""

--- src/main/java/jp/co/runy/sample/Car.java
+++ src/main/java/jp/co/runy/sample/Car.java
@@ -1,36 +1,40 @@
 package jp.co.runy.sample;
 
 public class Car {
-	private int speed;
-	private String bodyColor;
+  private int speed;//f/x1回目
+  private String bodyColor; //develop
+  
 
-	public Car() {
-	}
+  public Car() {
+	  // これが正しい
+  }
 
-	public Car(int speed2, String bodyColor) {
-		this.speed = speed2;
-		this.bodyColor = bodyColor;
-	}
+  public Car(int speed2, String bodyColor) {//f/x2かいめ
+    this.speed = speed2;
+    this.bodyColor = bodyColor;
+  }
 
-	public int getSpeed() {
-		return this.speed;
-	}
+  public int getSpeed() {
+    return this.speed;// b/x 2回目
+  }
 
-	public void setSpeed(int speed) {
-		this.speed = 10;
-	}
+  public void setSpeed(int speed) {
+    this.speed = 10;
+  }
 
-	public String getBodyColor() {
-		return this.bodyColor;
-	}
+  public String getBodyColor() {
+    return this.bodyColor; // test
+  }
 
-	public void setBodyColor(String bodyColor) {
-		this.bodyColor = bodyColor;
-	}
+  public void setBodyColor(String bodyColor) {
+    this.bodyColor = bodyColor;
+  }
 
-	@Override
-	public String toString() {
-		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
-	}
-
+  @Override
+  public String toString() {
+    return "{" +
+      " speed='" + getSpeed() + "'" +
+      ", bodyColor='" + getBodyColor() + "'" +
+      "}";
+  }
 }
